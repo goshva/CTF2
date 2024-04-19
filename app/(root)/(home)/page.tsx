cb96ca05a5943233b7b3e3fea5eb054bffeaa14d
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-04-19 22:44:53 +0400

    Добавления карточки профиля и фильтрации в sideBar

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const HomePage:NextPage = () => {
+  return (
+    <div>
+      Home Page
+    </div>
+  )
+}
+
+export default HomePage
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+import styles from "./layout.module.scss";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import News from "@/components/News";
+
+import backgroundImage from "@/main-bg.png";
+
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <div className="bgWrap">
+        <Image
+          src={backgroundImage}
+          alt="background Image"
+          fill
+          priority
+          placeholder="blur"
+          quality={100}
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.wrapper}>
+        <Header />
+
+        <Sidebar />
+
+        <News />
+
+        <main className={styles.main}>{children}</main>
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default HomeLayout;
