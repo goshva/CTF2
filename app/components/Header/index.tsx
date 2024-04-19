985bdcde5fb9d6c0b5ff836a4348c62e53f6e633
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-04-19 22:58:32 +0400

    pull from main

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,20 +1,20 @@
-import { FC } from 'react';
-import styles from './header.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
-import { ShoppingCart } from 'lucide-react';
+import { FC } from "react";
+import styles from "./header.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
 
-import logo from '@/logo.svg';
+import logo from "@/logo.svg";
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div>
-        <Link href={'/'}>
+        <Link href={"/"}>
           <Image src={logo} alt="logo" width={260} height={30} quality={100} />
         </Link>
 
-        <Link href={'/market'} className={styles.cartLink}>
+        <Link href={"/market"} className={styles.cartLink}>
           <ShoppingCart size={36} />
         </Link>
       </div>
