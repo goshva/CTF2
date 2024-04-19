21afe8c45f9782e6de07780983ee9a5d18e83cd9
LeonidKhoussid <leoforrussia@gmail.com>
2024-04-19 19:08:09 +0300

    added news carousel in header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -10,15 +10,14 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div>
-      <Link href={"/"}>
-        <Image src={logo} alt="logo" width={260} height={30} quality={100} />
-      </Link>
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width={260} height={30} quality={100} />
+        </Link>
 
-      <Link href={"/market"} className={styles.cartLink}>
-        <ShoppingCart size={36}/>
-      </Link>
+        <Link href={"/market"} className={styles.cartLink}>
+          <ShoppingCart size={36} />
+        </Link>
       </div>
-
     </header>
   );
 };
