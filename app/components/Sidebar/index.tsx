01f3f5ae3021e310cc3aa5e7372043057b7b7f3a
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-04-23 13:15:50 +0400

    добавления sharp libary и fix build

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -120,14 +120,6 @@ const Sidebar: FC = () => {
       </div>
 
       <div className={styles.middleSide}>
-        {/* <div className={styles.selectPole}>
-            <p>Тип товара (переименовать)</p>
-            <Image src={plusIcon} alt="decrement" />
-          </div>
-          <div className={styles.selectPole}>
-            <p>Критерий</p>
-            <Image src={plusIcon} alt="decrement" />
-          </div> */}
         <div className={styles.select_wrapper}>
           <section className={styles.select_section}>
             <Menu
@@ -135,86 +127,88 @@ const Sidebar: FC = () => {
               defaultSelectedKeys={['1']}
               defaultOpenKeys={['sub1']}
               mode="inline">
-              {/* <Menu.SubMenu
-              style={{ width: 256, background: 'none', border: 'none' }}
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}>
-              <div
-                style={{ paddingLeft: '24px', height: '24px' }}
-                className="ant-menu-submenu-title">
-                <span style={{ color: 'white' }}>Тип товара (переименовать)</span>
-                <Image src={iconState === 'plusIcon' ? plusIcon : minusIcon} alt="decrement" />
-              </div>
-            </Menu.SubMenu> */}
               <Menu.SubMenu
                 style={{ background: '#131d2c', width: '300px', color: 'white' }}
                 onTitleClick={toggleIcon}
-                expandIcon={
-                  <Image src={iconState === 'plusIcon' ? plusIcon : minusIcon} alt="decrement" />
-                }
-                title={<span style={{ color: 'white' }}>Тип товара (переименовать)</span>}>
+                title={
+                  <span
+                    style={{
+                      color: 'white',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}>
+                    <span>Тип товара (переименовать)</span>
+                    <Image src={iconState === 'plusIcon' ? plusIcon : minusIcon} alt="icon" />
+                  </span>
+                }>
                 <Menu.SubMenu
                   style={{ background: '#243766', width: '300px', color: 'white' }}
                   onTitleClick={toggleIconPistol}
-                  expandIcon={
-                    <Image
-                      src={pistolState === 'plusIcon' ? plusIcon : minusIcon}
-                      alt="decrement"
-                    />
-                  }
-                  title={<span style={{ color: 'white' }}>Пистолет</span>}>
+                  title={
+                    <span
+                      style={{
+                        color: 'white',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                      }}>
+                      <span>Пистолет</span>
+                      <Image src={pistolState === 'plusIcon' ? plusIcon : minusIcon} alt="icon" />
+                    </span>
+                  }>
                   <div className={styles.menuContainer}>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="13">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="13">
                       Все пистолеты
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="14">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="14">
                       Five-Seven
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="15">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="15">
                       Glock-18
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="16">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="16">
                       P2000
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="17">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="17">
                       P250
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="18">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="18">
                       R8 Revolver
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="19">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="19">
                       Tec-9
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="21">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="21">
                       CZ75-Auto
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="22">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="22">
                       USP-S
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="23">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="23">
                       Desert Eagle
                     </Menu.Item>
-                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} key="24">
+                    <Menu.Item style={{ color: 'white', background: '#5D6EAB' }} eventKey="24">
                       Dual Berettas
                     </Menu.Item>
                   </div>
                 </Menu.SubMenu>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="14">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="14">
                   Снайперская винтовка
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="15">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="15">
                   Пистолет-пулемет
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="16">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="16">
                   Пулемет
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="17">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="17">
                   Дробовик
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="18">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="18">
                   Нож
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="19">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="19">
                   Прочее
                 </Menu.Item>
               </Menu.SubMenu>
@@ -222,23 +216,28 @@ const Sidebar: FC = () => {
                 style={{ background: '#131d2c', width: '300px', color: 'white' }}
                 key="sub3"
                 onTitleClick={toggleIconSecond}
-                expandIcon={
-                  <Image
-                    src={iconStateSecond === 'plusIcon' ? plusIcon : minusIcon}
-                    alt="decrement"
-                  />
-                } // Используем условное выражение для определения иконки
-                title={<span style={{ color: 'white' }}>Критерий</span>}>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="14">
+                title={
+                  <span
+                    style={{
+                      color: 'white',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}>
+                    <span>Критерий</span>
+                    <Image src={iconStateSecond === 'plusIcon' ? plusIcon : minusIcon} alt="icon" />
+                  </span>
+                }>
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="14">
                   Категория
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="15">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="15">
                   Фазы
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="16">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="16">
                   Раритетность
                 </Menu.Item>
-                <Menu.Item style={{ color: 'white', background: '#243766' }} key="17">
+                <Menu.Item style={{ color: 'white', background: '#243766' }} eventKey="17">
                   Качество
                 </Menu.Item>
               </Menu.SubMenu>
