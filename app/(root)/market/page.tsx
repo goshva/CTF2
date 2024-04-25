79e199730c0655ed4007d98cdf822d849ff0d9a6
Андрей  Кочетов <andrei.glava03@gmail.com>
2024-04-26 01:12:04 +0400

    feat: add product page

---
+++ app/(root)/market/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const MarketPage:NextPage = () => {
+  return (
+    <div>
+      Market Page
+    </div>
+  )
+}
+
+export default MarketPage
