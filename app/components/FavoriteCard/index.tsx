270316c70d9393a4ead31e5c79732d4127d4e522
gemansipator <gemansipator@gmail.com>
2024-05-13 11:29:49 +0300

    Обновление предварительной версии страницы избранного

---
+++ app/components/FavoriteCard/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './favoritecard.module.scss';
+import PostPhoto from '@/Glock-18.png';
+import Image from 'next/image';
+
+const Favorite = () => {
+    return (
+        <div className={styles.message}>
+            <div className={styles.content}>
+
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src={PostPhoto} height={300} width={300} alt='post-photo'/>
+                <div>
+                    <strong>Название, цена</strong>
+                    <div className={styles.btn_sort}>
+                        <button>удалить</button>
+                        <button>в корзину</button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        // <div className={styles.message}>
+        //     <div className={styles.content}>
+        //         <div>
+        //             <strong>@Username</strong> liked your post <span>19h ago</span>
+        //         </div>
+        //     </div>
+        //     <div className={styles.imageContainer}>
+        //         <Image src={PostPhoto} height={100} width={100} alt='post-photo'/>
+        //     </div>
+        // </div>
+    );
+};
+
+export default Favorite;
