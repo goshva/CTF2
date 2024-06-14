e5c4438d903d83a0835f747eabc34d2b340f1351
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-06-14 14:40:33 +0400

    убрал use client с layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react';
 import Image from 'next/image';
 import styles from './layout.module.scss';
-import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import News from '@/components/News';
 
 // import backgroundImage from '@/main-bg.png';
 import backgroundImage from '../../public/background.jpg';
