c293c1c3b1317d6b2c929a0675654aa4dbe47c03
aleksei-shvetz <al.sv.dev@gmail.com>
2024-06-15 09:53:14 +0300

    Create fake data for market

---
+++ app/(root)/market/fakeData.ts
@@ -0,0 +1,279 @@
+import { IProduct } from '../../../types/index'
+
+const fakeData: IProduct[] = [
+  {
+    image: '/anubis.png',
+    alt: 'Product 1',
+    name: 'AK-47',
+    price: '454,98',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/speedy-gun.jpg',
+    alt: 'Product 2',
+    name: 'UZI',
+    price: '890,20',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/Glock-18.png',
+    alt: 'Product 3',
+    name: 'AK-47',
+    price: '94,50',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/anubis.png',
+    alt: 'Product 4',
+    name: 'AK-47',
+    price: '454,98',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/Glock-18.png',
+    alt: 'Product 5',
+    name: 'Glock-18',
+    price: '240,51',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/ak47-asiimov.jpg',
+    alt: 'Product 6',
+    name: 'AK47 ASIIMOV',
+    price: '740,00',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/AK47-ex.jpg',
+    alt: 'Product 7',
+    name: 'AK47-ex',
+    price: '240,51',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/tec-9.jpg',
+    alt: 'Product 8',
+    name: 'TEC-9',
+    price: '618,13',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/speedy-gun.jpg',
+    alt: 'Product 9',
+    name: 'Speedy GUN',
+    price: '388,40',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/ak47-asiimov.jpg',
+    alt: 'Product 10',
+    name: 'AK47 ASIIMOV',
+    price: '1740,00',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/anubis.png',
+    alt: 'Product 11',
+    name: 'UZI',
+    price: '990,20',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/tec-9.jpg',
+    alt: 'Product 12',
+    name: 'TEC-9',
+    price: '516,00',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/Glock-18.png',
+    alt: 'Product 13',
+    name: 'Glock-18',
+    price: '117,50',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/anubis.png',
+    alt: 'Product 13',
+    name: 'UZI',
+    price: '1190,80',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/ak47-asiimov.jpg',
+    alt: 'Product 14',
+    name: 'AK47 ASIIMOV',
+    price: '1200,00',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/AK47-ex.jpg',
+    alt: 'Product 15',
+    name: 'AK47-ex',
+    price: '776,60',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/Glock-18.png',
+    alt: 'Product 16',
+    name: 'Glock-18',
+    price: '458,60',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/tec-9.jpg',
+    alt: 'Product 17',
+    name: 'TEC-9',
+    price: '610,00',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/anubis.png',
+    alt: 'Product 18',
+    name: 'UZI',
+    price: '390,80',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/speedy-gun.jpg',
+    alt: 'Product 19',
+    name: 'Speedy GUN',
+    price: '88,40',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+  {
+    image: '/AK47-ex.jpg',
+    alt: 'Product 20',
+    name: 'AK47-ex',
+    price: '216,60',
+    sellers: [
+      {
+        image: '/box.svg',
+        alt: '',
+        count: 16,
+      },
+    ],
+  },
+];
+
+export default fakeData;
