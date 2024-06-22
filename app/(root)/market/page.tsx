5f773c9a6b7f9b88ea0014c669e3e0b048ec74d3
aleksei-shvetz <al.sv.dev@gmail.com>
2024-06-22 21:56:36 +0300

    Update fyles

--- app/(root)/market/page.tsx
+++ app/(root)/market/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -57,18 +57,18 @@ const ProductCard: React.FC<{ product: IProduct }> = ({ product }) => {
 };
 const MarketPage: NextPage = () => {
   // const { data = [] } = useGetAllProductsQuery();
-  const [filters, setFilters] = useState<IFilters>({
+  /* const [filters, setFilters] = useState<IFilters>({
     minPrice: 0,
     maxPrice: 2000,
     about15min: false,
     instantly: false,
     colors: [],
-  });
-  const stateFilters = useSelector(getFilters);
+  }); */
+  const filters = useSelector(getFilters);
 
-  useEffect(() => {
+  /* useEffect(() => {
     setFilters(stateFilters);
-  }, [stateFilters]);
+  }, [stateFilters]); */
 
   const renderCards = (productsData: IProduct[]) => {
     const cards: JSX.Element[] = productsData.filter((product) => priceRangeFilter(filters.minPrice, filters.maxPrice, product.price))
