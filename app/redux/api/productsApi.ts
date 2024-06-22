f9fb4ad1c008d2adaa02db516b94dd592511cab3
aleksei-shvetz <al.sv.dev@gmail.com>
2024-06-22 18:49:39 +0300

    Create products api

--- app/redux/api/productsApi.ts
+++ app/redux/api/productsApi.ts
@@ -1,25 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import build from 'next/dist/build';
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   tagTypes: ['Products'],
   baseQuery: fetchBaseQuery({
-    baseUrl: '',
+    baseUrl: 'https://www.ctforumtest.ru/v1/',
   }),
   endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: () => '',
-      providesTags: ({ results }) =>
-        results
-          ? [
-            ...results.map(({ id }: { id: string }) => ({
-              type: 'Products',
-              id,
-            })),
-            { type: 'Products', id: 'LIST' },
-          ]
-          : [{ type: 'Products', id: 'LIST' }],
+    getAllProducts: builder.query({
+      query: () => `product`,
     }),
   })
 });
+
+export const { useGetAllProductsQuery } = productsApi;
